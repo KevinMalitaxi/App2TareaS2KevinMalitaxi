2b8bffca25fdc11dc51d220153118a21b218227c
Kevin <Kevin@DESKTOP-TP54JO8>
2021-10-23 23:04:16 -0500

    Agregar archivos de proyecto.

---
+++ App2TareaS2KevinMalitaxi/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2TareaS2KevinMalitaxi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+           
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            String Usuario = txtUsuario.Text;
+            String Clave = txtClave.Text;
+
+            String user = "estudiante2021";
+            String pass = "uisrael2021";
+
+
+            try
+            {
+                if (Usuario == user)
+                {
+                    await Navigation.PushAsync(new ventanaDos(Usuario, Clave));
+
+                }
+                else if (Clave == pass)
+                {
+                    await Navigation.PushAsync(new ventanaDos(Usuario, Clave));
+
+                }
+                else if (Usuario != user && Clave != pass)
+                {
+                    DisplayAlert("ERROR DE INGRESO DE CREDENCIALES", "Usuario o Contraseña Incorrecta", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+    }
+}
---
+++ App2TareaS2KevinMalitaxi/ventanaDos.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2TareaS2KevinMalitaxi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ventanaDos : ContentPage
+    {
+        public ventanaDos(String usuario, String clave)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double Seg1 = Convert.ToDouble(txtValorUno.Text);
+                double examen1 = Convert.ToDouble(txtValorDos.Text);
+
+                double Seg2 = Convert.ToDouble(txtValorTres.Text);
+                double examen2 = Convert.ToDouble(txtValorCuatro.Text);
+
+                //Operaciones para Cálculo de Notas
+                double parcial1 = (Seg1 * 0.3) + (examen1 * 0.2);
+                double parcial2 = (Seg2 * 0.3) + (examen2 * 0.2);
+
+                double final = parcial1 + parcial2;
+
+
+                //Visualizacion del resultado en pantalla 
+                txtResultado1.Text = parcial1.ToString();
+                txtResultado2.Text = parcial2.ToString();
+                txtFinal.Text = final.ToString();
+
+
+                if (final >= 7)
+                {
+                    DisplayAlert("Resultado De Notas", "Felicidades el Estudiante se encuentra: APROBADO", "Salir");
+                }
+                else if (final >= 5 && final <= 6.99)
+                {
+                    DisplayAlert("Resultado De Notas", "El estudiante debe rendir el Examen COMPLEMENTARIO", "Salir");
+                }
+                else
+                {
+                    DisplayAlert("Resultado De Notas", "Lastimosamente el estudiante se encuentra: REPROBADO", "Salir");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Mensaje De Alerta", ex.Message, "Salir");
+            }
+
+        }
+    }
+    }
